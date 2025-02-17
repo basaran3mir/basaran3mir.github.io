2834eecf057280a745b6d53246f411077954abe5
basaran3mir <ebasaran999@gmail.com>
2025-02-17 17:06:32 +0300

    Personal Website R-V1
    Version: return v1

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const sidebar = document.getElementById("sidebar");
-    const menuToggle = document.getElementById("menu-toggle");
-    const menuLinks = document.querySelectorAll(".menu a");
+    const sidebar = document.getElementById("desktopSidebar");
+    const menuToggle = document.getElementById("menuToggle");
+    const menuLinks = document.querySelectorAll(".menu-link");
 
     menuLinks.forEach(link => {
         link.addEventListener('click', function () {
