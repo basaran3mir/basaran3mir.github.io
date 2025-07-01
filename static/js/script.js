a073b00ca47c5c13801076c19646a001819d5e85
basaran3mir <ebasaran999@gmail.com>
2025-07-01 09:12:47 +0300

    Start over
    All clear

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const sidebar = document.getElementById("desktopSidebar");
-    const menuToggle = document.getElementById("menuToggle");
+    const sidebar = document.getElementById("desktop-sidebar");
     const menuLinks = document.querySelectorAll(".menu-link");
 
     menuLinks.forEach(link => {
         link.addEventListener('click', function () {
             menuLinks.forEach(item => item.classList.remove('active'));
             link.classList.add('active');
-
-            if (window.innerWidth <= 768) {
-                sidebar.classList.remove("open");
-            }
         });
-    });
-
-    menuToggle.addEventListener("click", function () {
-        sidebar.classList.toggle("open");
     });
 });
